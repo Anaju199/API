8b11bb7c94569c074f3c8c8a0dcde098c0bcd076
AnaJulia <anajulia99@gmail.com>
2024-04-06 23:06:39 -0300

    Atualização config

--- setup/settings.py
+++ setup/settings.py
@@ -132,6 +132,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
 ]
 
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 DEFAULT_FROM_EMAIL = 'anajulia99@gmail.com'
 EMAIL_HOST = config('EMAIL_HOST')
