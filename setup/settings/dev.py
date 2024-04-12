131de5a1981e3eb5e28e321d725ea6493c8d5390
Ana Júlia <anajulia99@gmail.com>
2024-04-12 12:52:59 -0300

    Update dev.py

--- setup/settings/dev.py
+++ setup/settings/dev.py
@@ -1,6 +1,8 @@
 from .common import *
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 DEBUG = True
+
 SECRET_KEY = "django-insecure-u+2-)=zkma-vjz$xn#p)4$#&b-)u&g86k$7@+mo%uo1c5)llde"
 
